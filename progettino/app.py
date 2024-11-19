fc8ddae245580177deb016382c8f8fb911a8dacc
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-19 08:16:10 +0000

    fase 2

--- progettino/app.py
+++ progettino/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-#inizializza l'app Flask
+from flask import Flask, render_template
+#render_template: collegare file HTML.
+
+
+#base per far funzionare flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "primo step va"
-
+    render_template('index.html')
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
