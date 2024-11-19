e4e0466e6bd78666977c3488247be476aff4e1e5
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-19 08:29:56 +0000

    ora va fr

--- progettino/app.py
+++ progettino/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 #render_template: collegare file HTML.
 
-
 #base per far funzionare flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    render_template('index.html')
+    return render_template('index.html')
+
+listaSpesa=[]
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
