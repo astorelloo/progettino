7ab70ec6055ba98a1a9116559ef466ff241af289
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-19 08:44:22 +0000

    ho fatto il possibile

--- progettino/app.py
+++ progettino/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-listaSpesa=[]
+lista=["casa","macchina", "zio pera"]
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
