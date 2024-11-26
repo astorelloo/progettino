46b16af2cf096d18669cbd37f47837cffab4c1fe
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-26 08:15:09 +0000

    codice finito

--- progettino/app.py
+++ progettino/app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
-#render_template: collegare file HTML.
-
-#base per far funzionare flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+lista_spesa = ["casa", "macchina", "zio pera"]
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', lista=lista_spesa)
 
-lista=["casa","macchina", "zio pera"]
-#avvio dell'app Flask
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
     app.run(debug=True)
