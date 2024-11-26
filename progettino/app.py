bc8c5c8c9024a13cddab3f1c680b1fe2b1e8a170
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-26 08:46:59 +0000

    fase 3 quasi fatta

--- progettino/app.py
+++ progettino/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import flask_sqlalchemy
-#configurazione database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#configurazione del DB
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+#configurazione
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db.init_app(app)
+#crea il db se non esiste 
 with app.app_context():
     db.create_all()
 
-app = Flask(__name__)
 lista_spesa = ["casa", "macchina", "zio pera"]
 @app.route('/')
 def home():
+    ListaSpesa.query.all()
     return render_template('index.html', lista=lista_spesa)
 
 @app.route('/aggiungi', methods=['POST'])
--- progettino/models.py
+++ progettino/models.py
@@ -1,15 +1,4 @@
-#configurazione del DB
 from flask_sqlalchemy import flask_sqlalchemy
-app = Flask(__name__)
-
-#configurazione
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///lista_spesa.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-db.init_app(app)
-#crea il db se non esiste 
-with app.app_context():
-    db.create_all()
-#______________________________
 #creazione della tabella con le colonne
 db = SQLAlchemy() #inizialliziamo sqlAlchemy
 class ListaSpesa(db.Model):#tabella
