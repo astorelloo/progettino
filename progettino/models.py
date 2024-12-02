d8a4ce694866c9480fd1ed17b54a9ecb9bf26177
astorelloo astorelloo <lil.astor06@gmail.com>
2024-12-02 13:44:11 +0000

    css fatto

--- progettino/models.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-#creazione della tabella con le colonne
-db = SQLAlchemy() #inizialliziamo sqlAlchemy
-class ListaSpesa(db.Model):#tabella
-    id = db.Column(db.Integer, primary_key=True) #id unico  
-    elemento = db.Column(db.String(100), nullable=False) #elemento non nullo
